ed1482627de4951cea6478d1faf4060b65f1ca51
Ahmad Abugosh <ahmadabugosh@gmail.com>
2019-04-24 19:26:58 +0400

    Added registration page

--- src/routes/users/index.js
+++ src/routes/users/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './login';
+import Register from './register';
 import { isLoggedIn } from '../../helpers/authentication';
 import Dashboard from '../dashboard';
 
@@ -8,6 +9,7 @@ class App extends Component {
   render() {
     return (
       <Switch>
+        <Route path="/register" component={Register} />
         <Route path="/login" component={Login} />
         <Route path="" render={
           (props) => {
